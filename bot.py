23d19b4ad81248e455820f9a07b9e45aeab3aaa2
Rafael Nogales <xrnogales@gmail.com>
2021-01-21 17:03:57 +0100

    Refactor (style)

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ from settings import BOT_TOKEN
 import logging
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from yahoo_finance import YahooFinance
+
 import os
 
 yf = YahooFinance()
@@ -19,10 +22,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Mandame el excel \"Portfolio.xslx\" y generaré una imagen de tu cartera (de momento nada más)")
 
-def generate_picture(df):
-    df.index = df.Producto
-    img = df.plot.pie(y='Valor en EUR')
-    return img.figure
 
 def generate_circle_plot(df, path):
     # create data
@@ -45,22 +44,18 @@ def get_file(update, context):
     excel_file = update.message.document.get_file().download()
     df = pd.read_excel(excel_file)
     df['Ticker'] = df['Symbol/ISIN'].apply(yf.get_ticker)
-
     generate_circle_plot(df, './figure.png')
-
     context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('./figure.png', 'rb'))
 
     os.remove(excel_file)
     os.remove('./figure.png')
 
 
-from telegram.ext import MessageHandler, Filters
+start_handler = CommandHandler('start', start)
 excel_handler = MessageHandler(Filters.document, get_file)
 
-dispatcher.add_handler(excel_handler)
-
-start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(excel_handler)
 
 updater.start_polling()
 
