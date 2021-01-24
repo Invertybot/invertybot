6879fec8e4d7ef1393387f27d3e1970c48286d35
Rafael Nogales <xrnogales@gmail.com>
2021-01-24 13:27:44 +0100

    Set webhook

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import os
 import logging
 import pandas as pd
-import matplotlib.pyplot as plt
 
+import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
+from generate_plot import generate_circle_plot
 from yahoo_finance import YahooFinance
-from settings import BOT_TOKEN
+from settings import BOT_TOKEN, SERVER_URL, PORT
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -20,22 +21,6 @@ dispatcher = updater.dispatcher
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hola! Mandame el excel \"Portfolio.xslx\" y generaré una imagen de tu cartera (de momento nada más)")
-
-
-def generate_circle_plot(df, path):
-    # create data
-    names = df['Ticker']
-    size = df['Valor en EUR']
-
-    # Create a circle for the center of the plot
-    my_circle = plt.Circle((0, 0), 0.7, color='white')
-
-    plt.pie(size, labels=names)
-    p = plt.gcf()
-    p.gca().add_artist(my_circle)
-    # plt.show()
-    p.savefig(path)
-    return p
 
 
 def get_file(update, context):
@@ -58,5 +43,14 @@ excel_handler = MessageHandler(Filters.document, get_file)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(excel_handler)
 
-updater.start_polling()
+bot = telegram.Bot(token=BOT_TOKEN)
+bot.setWebhook(SERVER_URL + BOT_TOKEN)
+
+# updater.start_polling()
+
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=BOT_TOKEN)
+updater.bot.setWebhook(SERVER_URL + BOT_TOKEN)
+updater.idle()
 
---
+++ generate_plot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def generate_circle_plot(df, path):
+    # create data
+    names = df['Ticker']
+    size = df['Valor en EUR']
+
+    # Create a circle for the center of the plot
+    my_circle = plt.Circle((0, 0), 0.7, color='white')
+
+    plt.pie(size, labels=names)
+    p = plt.gcf()
+    p.gca().add_artist(my_circle)
+    # plt.show()
+    p.savefig(path)
+    return p
