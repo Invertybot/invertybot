3e99bb8725be237a5cc82634df9b5f3eb4491037
Rafael Nogales <xrnogales@gmail.com>
2021-01-24 17:46:58 +0100

    Run in heroku

--- api.py
+++ api.py
@@ -26,6 +26,6 @@ def get_global_portflio():
 
 if __name__ == '__main__':
 
-    # app.run(host='127.0.0.1', port=int(os.environ.get('PORT', 5000)), debug=True)
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    # app.run(host='127.0.0.1', port=5000, debug=True)
 
